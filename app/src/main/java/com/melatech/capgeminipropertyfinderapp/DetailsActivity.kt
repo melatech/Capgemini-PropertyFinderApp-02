d825672d521fcf20fb75853bb9d9d398279828de
melatech <maincontractor@gmail.com>
2022-03-31 14:47:27 +0100

    Sixth commit: ui button features: implementbuttons with Intent() object

--- app/src/main/java/com/melatech/capgeminipropertyfinderapp/DetailsActivity.kt
+++ app/src/main/java/com/melatech/capgeminipropertyfinderapp/DetailsActivity.kt
@@ -2,10 +2,16 @@ package com.melatech.capgeminipropertyfinderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailsActivity : AppCompatActivity() {
+    lateinit var backButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        backButton = findViewById(R.id.backButton)
+
+        backButton.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/com/melatech/capgeminipropertyfinderapp/MainActivity.kt
+++ app/src/main/java/com/melatech/capgeminipropertyfinderapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.melatech.capgeminipropertyfinderapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+    lateinit var card1: LinearLayout
+    lateinit var card2: LinearLayout
+    lateinit var card3: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        card1 = findViewById(R.id.card_1)
+        card2 = findViewById(R.id.card_2)
+        card3 = findViewById(R.id.card_3)
+
+        card1.setOnClickListener(this)
+        card2.setOnClickListener(this)
+        card3.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val intent = Intent(this, DetailsActivity::class.java)
+
+        startActivity(intent)
     }
 }
